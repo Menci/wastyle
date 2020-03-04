0614391531b2d2a1af27391bfca5a8edc2624ea2
Menci <huanghaorui301@gmail.com>
2020-03-04 11:25:12 +0000

    test: add test

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const expect = require("chai").expect;
+const wastyle = require("..");
+
+it("Formatter should work", async () => {
+  await wastyle.init(fs.readFileSync(require.resolve("../dist/astyle.wasm")));
+
+  const code = "#include <cstdio>\nint main(){int a,*b=a,c,*d=nullptr;return -1;}";
+  const options = "pad-oper style=google";
+
+  const [success, result] = wastyle.format(code, options);
+  expect(success).to.be.true;
+  expect(result).to.be.equals(
+    "#include <cstdio>\nint main() {\n    int a, *b = a, c, *d = nullptr;\n    return -1;\n}"
+  );
+});
